7e9faa0e4cfb5fdd21a2d8a8fea5c32b6b266495
AR-Shahin <mdshahinmije96@gmail.com>
2021-12-11 12:42:50 +0600

    database

---
+++ database/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100)->default('Shahin')->unique();
+            // $table->integer('vote', 10)->default(2);
+            $table->text('address');
+            $table->boolean('is_admin')->default(false);
+            $table->enum('role', ['admin', 'user', 'visitor']);
+            $table->float('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
