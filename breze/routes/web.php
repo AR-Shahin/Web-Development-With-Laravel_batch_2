3fea71dc56f06d1b3afa721e92172eff73336a6d
AR-Shahin <mdshahinmije96@gmail.com>
2022-01-15 21:47:53 +0600

    auth

---
+++ breze/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.auth.dashboard');
+})->middleware(['auth:admin'])->name('admin.dashboard');
+
+require __DIR__ . '/auth.php';
